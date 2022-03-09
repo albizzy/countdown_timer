68f8edfa49283384f2736337a76097638b232f68
Albert Mwasisoba <almwassy@gmail.com>
2022-03-09 12:30:31 +0300

    second commit

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const countdown = () => {
-    const countDate = new Date('March 31, 2022 00:00:00');
-    console.log(countDate);
+    const finalDate = new Date('March 31, 2022 00:00:00').getTime();
+    const currentTime = new Date().getTime();
+    const timeGap = finalDate - currentTime;
+
+    //How time works - muda unafanya vipi kazi
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    //calculated time
+    const textDay = Math.floor(timeGap / day);
+    console.log(textDay)
 };
 
 countdown();
